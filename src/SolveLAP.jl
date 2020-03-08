8b61e4566cc3239dd45fac4425bec789a56870ce
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-09 00:17:23 +0100

    add doc string for stiff problem solver

--- src/SolveLAP.jl
+++ src/SolveLAP.jl
@@ -52,6 +52,31 @@ function solve_lap(costMat::AbstractMatrix)
     return assignment,cost
 end
 
+"""
+solve_stiff_lap(cost_matrix) -> assignment in cartesian coords
+
+# Example
+```julia
+julia> using SolveLAP
+
+julia> M=rand([1,2,Inf],5,5)
+5×5 Array{Float64,2}:
+ Inf    Inf      1.0    1.0    1.0
+ Inf    Inf    Inf    Inf    Inf
+   2.0    1.0  Inf      1.0    2.0
+ Inf      2.0    1.0    2.0  Inf
+   2.0    2.0    2.0  Inf      1.0
+
+julia> solve_stiff_lap(M)
+6-element Array{Tuple{Int64,Int64},1}:
+ (1, 4)        ← row 1 is assigned to column 4
+ (2, -1)       ← -1 for missing assignment, row 2 is assigned to nothing
+ (3, 2)
+ (4, 3)
+ (5, 5)
+ (-1, 1)       ← column 1 is assigned to nothing
+```
+"""
 function solve_stiff_lap(C::AbstractMatrix{T}, penalty=1.05) where T <: Real
     m,n=size(C)
     assignment,cost=solve_lap(StiffWrapper(C,penalty))
