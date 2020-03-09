0b4f2ca7619a0a0cfa926fd52ba1b7546123a693
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-09 11:41:58 +0100

    fix

--- src/SolveLAP.jl
+++ src/SolveLAP.jl
@@ -1,5 +1,5 @@
 module SolveLAP
-##### PLEASE READ THE NOTICE ON THE BOTTOM, for the original license and authors
+##### PLEASE READ THE NOTICE ON THE BOTTOM, to know the original authors of this code
 using SparseArrays
 export solve_lap, solve_stiff_lap
 include("StiffWrapper.jl")
@@ -429,30 +429,7 @@ end
 #### NOTICE!!!
 #### most functions in this file are taken with almost no modifications from https://github.com/Gnimuc/Hungarian.jl
 #### credit for those functions go to the original contributors: Yupei Qi <qiyupei@gmail.com>, et al
+#### LICENSE is the Unlicense @ https://github.com/Gnimuc/Hungarian.jl/blob/0af029c5bc25bdff8d75296f9ae510e3f236f0aa/LICENSE
 #### --------------------------------------------------------------------------------------------------------
-
-###### ORIGINAL LICENSE
-# MIT License
-#
-# Copyright (c) 2017 Yupei Qi and other contributors
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 
 end # module
